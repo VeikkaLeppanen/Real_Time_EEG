2308c1b747c4a83b7c89180162a17a7f12e6c5c4
Veikka <veikka.leppanen@aalto.fi>
2025-02-21 14:23:34 +0200

    Removing old files

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -r build
-mkdir build
-
-g++ -fopenmp $(pkg-config --cflags python3) -o build/datastream main.cpp eeg_bridge/eeg_bridge.cpp eeg_bridge/measurementStartPacket.cpp eeg_bridge/samplePacket.cpp eeg_bridge/networkUtils.cpp dataHandler/dataHandler.cpp dataHandler/GACorrection.cpp -lpython3.11 -I/usr/include/eigen3 -I/usr/lib/python3.11/site-packages/numpy/core/include -Wno-deprecated-declarations
-
-./build/datastream
